2c01b2bb953a2242ff10faa94282bfadca0d0077
Farzaal <farzalkhan786@gmail.com>
2020-03-05 01:15:47 +0500

    join room params

--- src/utilities/socketEvents.js
+++ src/utilities/socketEvents.js
@@ -5,20 +5,19 @@ module.exports = function (io) {
 
     io.on('connection', function (socket) {
 
-        socket.on('joinRoom', async () => {
+        socket.on('joinRoom', async ({ room_id }) => {
             const decoded = jwt.decode(socket.handshake.query.token, { complete: true });
-            socket.join(socket.handshake.query.room_id);
+            socket.join(room_id);
             await roomService.updUserSocketId(decoded.payload.uid, socket.id);
-            const usrMsg = await roomService.getUserMessages(socket.handshake.query.room_id);
+            const usrMsg = await roomService.getUserMessages(room_id);
             io.to(socket.id).emit('joinRoom', usrMsg);
         });
 
-        socket.on('message', async (userMessage) => {
+        socket.on('message', async ({ room_id, message }) => {
             try {
-                const { user_id, message } = userMessage;
                 const decoded = jwt.decode(socket.handshake.query.token, { complete: true });
-                io.to(socket.handshake.query.room_id).emit('message', [userMessage]);
-                await roomService.saveRoomMessage({ room_id: socket.handshake.query.room_id, user_id: user_id, message: message });
+                io.to(room_id).emit('message', [message]);
+                await roomService.saveRoomMessage({ room_id, user_id: decoded.payload.uid, message });
             } catch(e) {
                 console.log(e);
             }
